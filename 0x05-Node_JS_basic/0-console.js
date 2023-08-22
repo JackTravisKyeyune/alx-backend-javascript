d941e86130b7ff7e201d5ecd3c1d42f0474f802f
JackTravisKyeyune <juliesenynam@gmail.com>
2023-08-22 19:01:20 +0000

    Node js basics

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Jack Travis Kyeyune <https://github.com/JackTravisKyeyune>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
